a67b5269af913acab9a50f1c94184779678dbe7b
niyazone1 <niyazone1@gmail.com>
2023-05-26 22:41:14 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/digital/taranila/nothing_browser/MainActivity.kt
@@ -0,0 +1,6 @@
+package digital.taranila.nothing_browser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nothing Browser',
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark,
+
+      home: const DashboardPage(),
+    );
+  }
+}
