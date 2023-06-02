32cba1c58208d0a4fa3bdc102850bd1e90e721f0
niyazone1 <niyazone1@gmail.com>
2023-06-02 22:31:09 +0530

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nothing_browser/dashboard.dart';
-import 'package:nothing_browser/pages/aol.dart';
-import 'package:nothing_browser/pages/ask.dart';
-import 'package:nothing_browser/pages/bing.dart';
-import 'package:nothing_browser/pages/duck.dart';
-import 'package:nothing_browser/pages/ecosia.dart';
-import 'package:nothing_browser/pages/google.dart';
-import 'package:nothing_browser/pages/start.dart';
-import 'package:nothing_browser/pages/wolfarm.dart';
-import 'package:nothing_browser/pages/yahoo.dart';
-import 'package:nothing_browser/pages/yandex.dart';
-
-
 
 void main() {
   runApp(const MyApp());
@@ -19,12 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -43,16 +25,6 @@ class MyApp extends StatelessWidget {
 
       routes: {
         '/': (context) => const DashboardPage(),
-        '/duckduck': (context) => const DuckPage(),
-        '/aold': (context) => const AolPage(),
-        '/askd': (context) => const AskPage(),
-        '/bingd': (context) => const BingPage(),
-        '/ecosiad': (context) => const EcosiaPage(),
-        '/googled': (context) => const GooglePage(),
-        '/startd': (context) => const StartPage(),
-        '/wolfarmd': (context) => const WolfarmPage(),
-        '/yahood': (context) => const YahooPage(),
-        '/yandexd': (context) => const YandexPage(),
 
 
 
